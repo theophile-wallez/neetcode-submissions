54b7afb5065e872c7aa538d0f8e9218191fb6ca1
Théophile Wallez <theophile.wall@gmail.com>
2026-06-04 09:53:17 +0200

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const store = new Map<string, string[]>();
+
+        strs.forEach(str => {
+            const signature = str.split('').reduce<number[]>((acc, curr) => {
+                const index = curr.charCodeAt(0) - 97;
+                acc[index] = (acc[index] ?? 0) + 1;
+                return acc;
+            }, new Array<number>(26).fill(0));
+            const sigStr = signature.join('/');
+
+            const currentStr = store.get(sigStr) ?? [];
+            const newStr = [...currentStr, str];
+
+            store.set(sigStr, newStr)
+        })
+
+        const res = []
+
+        for(const [sig, vals] of store) {
+            res.push(vals);
+        }
+
+        return res;
+    }
+}
