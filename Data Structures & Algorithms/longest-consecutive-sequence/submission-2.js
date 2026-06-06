399df1d1ade868edfdaaa3321391f05987c1ec2f
Théophile Wallez <theophile.wall@gmail.com>
2026-06-06 15:18:14 +0200

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const set = new Set(nums);
+        let longest = 0;
+
+        nums.forEach((num, index) => {
+            if (set.has(num - 1)) return;
+            let cursor = 1;
+            while (set.has(num + cursor)) {
+                cursor += 1;
+            }
+
+            longest = Math.max(longest, cursor);
+        });
+
+        return longest;
+    }
+}
